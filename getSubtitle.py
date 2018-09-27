99e518ef7c3335c4091c7dd04844e3e78f4bafb9
thekaleidoscope <yadhuksp@gmail.com>
2018-09-28 01:46:05 +0530

    Limited language to en and saved file at cwd

--- getSubtitle.py
+++ getSubtitle.py
@@ -3,7 +3,8 @@ from getLink import link
 import sys
 path = link("https://www.youtube.com/results?search_query=" +
             "+".join(sys.argv[1].split(" ")))
-print(path)
-
-os.system("youtube-dl --all-subs --write-auto-sub --skip-download "+path)
-print("done")
+# print(path)
+cwd = os.getcwd()
+os.system(
+    "youtube-dl --all-subs --write-sub --sub-lang \"en\" --skip-download -o \"{}\" {}".format(cwd, path))
+# print("done")
