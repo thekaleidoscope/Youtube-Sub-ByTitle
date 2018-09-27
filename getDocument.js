cb3ee273f49a0bdc74273935baba9e074575c77d
thekaleidoscope <yadhuksp@gmail.com>
2018-09-27 23:47:16 +0530

    getting the base website

--- getDocument.js
+++ getDocument.js
@@ -1,21 +1,39 @@
 var http = require("http");
 
 let path = process.argv[2].split(" ").join("+");
-console.log(path);
-// var options = {
-//   host: "https://www.youtube.com/results?search_query=",
-//   path: path
-// };
-// var request = http.request(options, function(res) {
-//   var data = "";
-//   res.on("data", function(chunk) {
-//     data += chunk;
-//   });
-//   res.on("end", function() {
-//     console.log(data);
-//   });
-// });
-// request.on("error", function(e) {
-//   console.log(e.message);
-// });
-// request.end();
+// console.log(path);
+var options = "https://www.youtube.com/results?search_query=" + path;
+const getScript = url => {
+  return new Promise((resolve, reject) => {
+    const http = require("http"),
+      https = require("https");
+
+    let client = http;
+
+    if (url.toString().indexOf("https") === 0) {
+      client = https;
+    }
+
+    client
+      .get(url, resp => {
+        let data = "";
+
+        // A chunk of data has been recieved.
+        resp.on("data", chunk => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          resolve(data);
+        });
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+};
+
+(async url => {
+  console.log(await getScript(url));
+})(options);
