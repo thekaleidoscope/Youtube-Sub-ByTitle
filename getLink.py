0342e313910bc9ef04b524625f0b3b3cba4fe4e0
thewisenerd <thewisenerd@protonmail.com>
2018-10-22 22:11:56 +0530

    rewrite interface, parsing, use python module for youtube-dl
    update imports

--- getLink.py
+++ getLink.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 import requests
@@ -16,3 +17,45 @@ def link(link):
         videolist.append(tmp)
 
     return videolist[0]
+
+def get_video_meta(dom):
+    content = dom.find('div', class_='yt-lockup-content')
+    dtitle = content.find('a', class_='yt-uix-tile-link')
+    dchannel = content.find_all('a', href=re.compile("^\/channel\/"))
+
+    if len(dchannel) == 0:
+        return None
+
+    cc = False
+    container = dom.find('div', class_='yt-lockup-badges')
+    if container != None:
+        badges = container.find('li', class_='yt-badge-item')
+        for badge in badges:
+            if badge.text == 'CC':
+                cc = True
+
+    return [dtitle.text, dchannel[0].text, dtitle['href'], cc]
+
+def parse(dom):
+    # div#results
+    # -- ol.item-section
+    # -- -- div.yt-lockup.yt-lockup-video
+    # -- -- -- div.yt-lockup-badges
+    # -- -- -- -- li.yt-badge-item
+    # -- -- -- -- -- span.yt-badge == "CC"
+    ret = []
+
+    results = dom.find('div', id='results')
+    isection = results.find('ol', class_='item-section')
+    videos = isection.select('div.yt-lockup.yt-lockup-video')
+
+    ret = list(map(get_video_meta, videos))
+
+    return [v for v in ret if v is not None]
+
+def links(link):
+    req = requests.get(link)
+    page = req.text
+    soup = bs(page, 'html.parser')
+
+    return parse(soup)
--- getSubtitle.py
+++ getSubtitle.py
@@ -1,13 +1,130 @@
+from __future__ import unicode_literals
+
 import os
+import argparse
 import sys
+import time
 
-from getLink import link
+import colorama
+import youtube_dl
+from colorama import Fore, Style
 
-path = link("https://www.youtube.com/results?search_query=" +
-            "+".join(sys.argv[1].split(" ")))
+import getLink
 
-# print(path)
-cwd = os.getcwd()
-os.system(
-    "youtube-dl --all-subs --write-sub --sub-lang \"en\" --skip-download -o \"{}\" {}".format(cwd, path))
-# print("done")
+parser = argparse.ArgumentParser()
+parser.add_argument('QUERY', nargs='+', help='one search query that can span either a single or multiple arguments')
+parser.add_argument('-l', '--lucky', help='select first CC\'d track', action='store_true')
+parser.add_argument('-c', '--convert', help='convert vtt to srt', action='store_true')
+parser.add_argument('-k', '--keep', help='keep original vtt after convert to srt (depends on -c/--convert)', action='store_true')
+parser.add_argument('-f', '--force-convert', help='overwrite existing vtt if any (implies -c/--convert)', action='store_true')
+
+ydl_opts = {
+    'subtitleslangs': ['en'],
+    'writesubtitles': True,
+    'skip_download': True,
+
+    'ignoreerrors': True
+}
+
+def convert_vtt(filename, force):
+    (fn, fe) = os.path.splitext(filename)
+    nargs = ['ffmpeg']
+    if force:
+        nargs.append('-y')
+    nargs.append('-i')
+    nargs.append(filename)
+    nargs.append('{}.srt'.format(fn))
+
+    sys.stdout.flush()
+    ret = os.spawnvp(os.P_WAIT, nargs[0], nargs)
+
+    if ret != 0:
+        print('convert fail, exit code: {:d}'.format(ret))
+        exit(ret)
+
+def download(args, convert=False, keep=False, force=False):
+    (title, channel, href, _) = args
+
+    dim = Style.DIM
+    reset = Style.RESET_ALL
+
+    print('+ {}{}{} :: {}'.format(dim, channel, reset, title))
+
+    url = 'https://www.youtube.com' + href
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ret = ydl.extract_info(url)
+
+        if ret != None:
+            if convert == True:
+                filename = ydl.prepare_filename(ret)
+                (fn, fe) = os.path.splitext(filename)
+                ext = '.en.vtt'
+                fn = fn + ext
+                fn = os.path.join(os.getcwd(), fn)
+                if os.path.exists(fn) and os.path.isfile(fn):
+                    convert_vtt(fn, force)
+                if not keep:
+                    os.unlink(fn)
+
+def real_main(query, lucky, convert, keep, force):
+    base = 'https://www.youtube.com/results?search_query='
+    qstr = '+'.join(query)
+
+    colorama.init()
+
+    videos = []
+    try:
+        videos = getLink.links(base+qstr)
+    except:
+        print(Fore.RED + 'error querying youtube' + Style.RESET_ALL)
+        exit(1)
+    cc = list(filter(lambda x : x[3], videos))
+
+    if len(cc) == 0:
+        print(Fore.RED + 'no videos found with CC' + Style.RESET_ALL)
+        exit(1)
+
+    if (lucky):
+        return download(cc[0], convert, keep, force)
+
+    fmt = '{:d}'
+    if len(cc) > 10:
+        fmt = '{:02d}'
+    for (i,e) in enumerate(cc):
+        (title, channel, href, _) = e
+
+        dim = Style.DIM
+        reset = Style.RESET_ALL
+
+        print((fmt + ' :: {}{}{} :: {}').format(i, dim, channel, reset, title))
+
+    uinp = -1
+    while uinp == -1:
+        try:
+            print('\nenter choice: ', end='')
+            uinp = int(input())
+        except:
+            # screw you.
+            exit(1)
+
+    if uinp >= len(cc):
+        # screw you too
+        exit(1)
+
+    return download(cc[uinp], convert, keep, force)
+
+def main(args):
+    args = vars(args)
+
+    flat = []
+    query = args['QUERY']
+    for sw in query:
+        for w in sw.split(' '):
+            if w != '':
+                flat.append(w)
+
+    real_main(flat, args['lucky'], (args['convert'] or args['force_convert']), args['keep'], args['force_convert'])
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
